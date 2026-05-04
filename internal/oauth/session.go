6d03cabad0c81a94c10829e2b8c63ef945be05ea
deR0R0 <rorozee1@gmail.com>
2026-05-04 15:19:39 -0400

    possibly get oauth sessions working

--- internal/oauth/session.go
+++ internal/oauth/session.go
@@ -1,6 +1,10 @@
 package oauth
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log/slog"
 	"os"
 
 	"golang.org/x/oauth2"
@@ -16,3 +20,85 @@ var config = &oauth2.Config{
 	},
 }
 
+type Session struct {
+	State string;
+	RedirectURI string;
+	Token *oauth2.Token;
+}
+
+type ManagerStruct struct {
+	config *oauth2.Config;
+	sessions map[int]*Session // <state>: <session_struct>
+}
+
+var manager* ManagerStruct // store a file-wide manager
+
+func init() {
+	manager = &ManagerStruct{
+		config: config,
+		sessions: make(map[int]*Session),
+	}
+}
+
+func Manager() *ManagerStruct {
+	return manager
+}
+
+func (m *ManagerStruct) getAuthURI(state *string) (string, error) {
+	if state == nil {
+		return "", fmt.Errorf("state is empty")
+	}
+
+	return m.config.AuthCodeURL(*state, oauth2.AccessTypeOffline), nil
+}
+
+func generateState() (string, error) {
+    bytes := make([]byte, 16)
+    if _, err := rand.Read(bytes); err != nil {
+        return "", err
+    }
+    return hex.EncodeToString(bytes), nil
+}
+
+func (m *ManagerStruct) CreateNewSession(userID int) (*Session, error) {
+	state, err := generateState()
+
+	if err != nil {
+		slog.Error("issue while generating state")
+		return nil, err
+	}
+
+	// generate the auth uri
+	authURI, err := m.getAuthURI(&state)
+
+	if err != nil {
+		slog.Error("issue while generating uri")
+		return nil, err
+	}
+
+	// actually create our session. token is nil until we get our token
+	session := &Session{
+		State: state,
+		RedirectURI: authURI,
+		Token: nil,
+	}
+
+	// add the session to our manager 
+	m.sessions[userID] = session
+
+	return session, nil
+}
+
+func (m *ManagerStruct) GetSession(userID int) (*Session, error) {
+	session, ok := m.sessions[userID]
+
+	if !ok {
+		return nil, fmt.Errorf("couldn't get session")
+	}
+
+	return session, nil
+}
+
+func (m *ManagerStruct) DeleteSession(userID int) {
+	delete(m.sessions, userID)
+}

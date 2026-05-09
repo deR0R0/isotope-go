acf76b3d7c52e73a6bf7815d3b2235083ed6d3ae
Roro Zee <rorozee1@Roros-MBP.fios-router.home>
2026-05-09 15:05:29 -0400

    add better error hadnling and auto delete login message

--- internal/commands/login.go
+++ internal/commands/login.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"log/slog"
+	"time"
 
 	"github.com/deR0R0/isotope-go/internal/oauth"
 	"github.com/disgoorg/disgo/discord"
@@ -21,7 +22,17 @@ func handleLogin(event *events.ApplicationCommandInteractionCreate) {
 
 	session, err := oauth.Manager().CreateNewSession(event.User().ID.String())
 
-	_, err = event.Client().Rest.CreateFollowupMessage(
+	if err != nil {
+		slog.Info("sending err message to user and stopping rest of command logic...")
+		event.Client().Rest.CreateFollowupMessage(
+			event.ApplicationID(),
+			event.Token(),
+			discord.NewMessageCreate().WithContent("Internal Server Error"),
+		)
+		return
+	}
+
+	msg, err := event.Client().Rest.CreateFollowupMessage(
 		event.ApplicationID(),
 		event.Token(),
 		discord.NewMessageCreate().WithContent(session.RedirectURI),
@@ -29,5 +40,13 @@ func handleLogin(event *events.ApplicationCommandInteractionCreate) {
 
 	if err != nil {
 		slog.Info("error while sending response: ", slog.Any("err", err))
+		return
 	}
+
+	go func() {
+		time.Sleep(time.Second * 5)
+		if err := event.Client().Rest.DeleteFollowupMessage(event.ApplicationID(), event.Token(), msg.ID); err != nil {
+			slog.Warn("issue with deleting followup message. message is probably already deleted, so safe to ignore this message.")
+		}
+	}()
 }

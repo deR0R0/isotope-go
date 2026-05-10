39f2d34197a4f9a815bd9d1808cc4c60f39e6c9c
deR0R0 <rorozee1@gmail.com>
2026-05-10 16:48:50 -0400

    whoops, forgot to close rows after query

--- internal/db/helper.go
+++ internal/db/helper.go
@@ -4,10 +4,12 @@ import "database/sql"
 
 func GetTokenData(db *sql.DB, userid string) (access_token string, token_type string, refresh_token string, expiry string, err error) {
 	rows, err := db.Query(`SELECT access_token, token_type, refresh_token, expiry FROM users WHERE id = ?`, userid)
-
+	
 	if err != nil {
 		return "", "", "", "", err
 	}
+
+	defer rows.Close()
 
 	var at, tt, rt, e string
 
